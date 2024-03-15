33ed62de26c0dfdd89e36511242cb736cd99f70a
AEC00 <163497365+AEC00@users.noreply.github.com>
2024-03-15 10:48:56 +0530

    Update Third.cs

--- Third.cs
+++ Third.cs
@@ -4,17 +4,15 @@ public class ColorChanger : MonoBehaviour
 {
 // List of colors
 public List<Color> colors;
-private int currentColorIndex = 0; //current color
+private int currentColorIndex = 0; 
 void Update()
 {
-if (Input.GetMouseButtonDown(0)) //check for mouse click
+if (Input.GetMouseButtonDown(0)) 
 {
 Vector2 mousePosition =
-Camera.main.ScreenToWorldPoint(Input.mousePosition);//get mouse position
-RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero); //Cast
-ray from mouse position
-if (hit.collider != null && hit.transform == this.transform) //check if ray
-hits square
+Camera.main.ScreenToWorldPoint(Input.mousePosition);
+RaycastHit2D hit = Physics2D.Raycast(mousePosition, Vector2.zero); 
+if (hit.collider != null && hit.transform == this.transform) 
 {
 // Change to the next color in the list
 currentColorIndex = (currentColorIndex + 1) % colors.Count;
